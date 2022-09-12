11417f210ee6771d05696d95ca0b4c3cf5dd961e
emsleyd <emsleyd@gmail.com>
2022-09-12 16:12:46 +0100

    attempting to get multer working

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-const upload = multer({ dest: './public/data/uploads/' })
+const upload = multer({ dest: "./public/uploads"})
 app.post('/stats', upload.single('uploaded_file'), function (req, res) {
    console.log(req.file, req.body)
 });
@@ -20,23 +20,6 @@ app.post('/stats', upload.single('uploaded_file'), function (req, res) {
 //image upload with multer
 
     app.post('/profile', upload.single('avatar'), function (req, res, next) {
-    //   // req.file is the `avatar` file
-    //   // req.body will hold the text fields, if there were any
-    })
-
-    app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
-      // req.files is array of `photos` files
-      // req.body will contain the text fields, if there were any
-    })
-
-    const cpUpload = upload.fields([{ name: 'gallery', maxCount: 100 }])
-    app.post('/cool-profile', cpUpload, function (req, res, next) {
-      // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
-      //
-      // e.g.
-      //  req.files['gallery'] -> Array
-      //
-      // req.body will contain the text fields, if there were any
     })
 
 
