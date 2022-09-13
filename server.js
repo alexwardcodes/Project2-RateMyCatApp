e0fcae8d6786c8d0de6b4ec76148b265b9053075
EHCarr <ash100.singh@gmail.com>
2022-09-13 09:11:12 +0100

    trying to get user login working

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 const catRouter = require("./routes/cats");
 const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
 
 // app.use(express.static("public"));
 app.use(expressLayouts);
